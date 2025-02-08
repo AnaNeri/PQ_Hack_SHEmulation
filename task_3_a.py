aa8bf0824cc539e4caa7c540dcc70ac565018aee
sawa1312 <114869882+sawa1312@users.noreply.github.com>
2025-02-08 21:18:52 +0000

    Create task_3_a.py

---
+++ task_3_a.py
@@ -0,0 +1,80 @@
+from qadence import (feature_map, hea, Z, QuantumModel, add, QuantumCircuit, 
+                     kron, FeatureParameter, RX, RZ, VariationalParameter, RY,
+                     chain, CNOT, X, Y, CRX, CRZ, I)
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit, minimize
+import torch
+
+#Constant and boundary conditions
+k = 2.3
+x0 = 1.0  # x(0) = 1.0
+dx0 = 1.2  # x'(0) = 1.2
+
+def vqc_fit(n_qubits, n_epochs):
+    
+    t = FeatureParameter("t")
+    omega = VariationalParameter("omega")
+    phi = VariationalParameter("phi")
+    C = VariationalParameter("C")
+    
+    block = chain(RX(0, omega * t + phi))
+    obs = C * Z(0)
+        
+    circuit = QuantumCircuit(1, block)
+    model = QuantumModel(circuit, observable = obs)
+
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.1)
+
+    for epoch in range(n_epochs):
+        optimizer.zero_grad()
+        loss = loss_fn(model, t_range, x0, dx0, k)
+        loss.backward()
+        optimizer.step()
+        y_pred = model.expectation({"t": t_range}).squeeze().detach()
+
+    return model, y_pred
+    
+#Since we don't have the training data, we need to define a different loss function based on the diff equation
+
+t_range = torch.linspace(0, 10, 100)  # range of t values
+
+def loss_fn(model, t_range, x0, dx0, k):
+    
+    t_range.requires_grad = True
+    x_t = model.expectation({"t": t_range}).squeeze()
+    
+    # dx/dt
+    dx_dt = torch.autograd.grad(x_t.sum(), t_range, create_graph=True)[0]
+    
+    # d^2x/dt^2
+    d2x_dt2 = torch.autograd.grad(dx_dt.sum(), t_range, create_graph=True)[0]
+    
+    # Check if learned derivative is behaving according to diff eq.
+    residual = d2x_dt2 + k**2 * x_t
+    
+    # Enforce boundary conditions
+    bc1 = x_t[0] - x0  # x(0) = 1.0
+    bc2 = dx_dt[0] - dx0  # x'(0) = 1.2
+    
+    # Total loss: MSE of residual and boundary conditions
+    loss = torch.mean(residual**2) + bc1**2 + bc2**2
+    return loss
+
+def plot(t_range, y_pred):
+    plt.plot(t_range.detach().numpy(), y_pred, label = "Prediction")
+    plt.xlabel("x")
+    plt.ylabel("x(t)")
+    plt.legend()
+    plt.show()
+
+quantum = True
+show = True
+
+if quantum: 
+    n_qubits = 1
+    model, y_pred = vqc_fit(n_qubits, n_epochs = 100)
+    if show:
+        plot(t_range, y_pred)
+    vparams = model.vparams
